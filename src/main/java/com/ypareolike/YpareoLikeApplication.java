f264fb478998513ae78cab6ae42495fa4298a9fd
Chéridanh <divintsiela@gmail.com>
2025-08-05 10:13:59 +0200

    first commit after creating project

---
+++ src/main/java/com/ypareolike/YpareoLikeApplication.java
@@ -0,0 +1,13 @@
+package com.ypareolike;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YpareoLikeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YpareoLikeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ypareolike/YpareoLikeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ypareolike;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YpareoLikeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
