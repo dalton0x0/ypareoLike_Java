8282660a3d798505616f046771b1a2ee03c0938a
Chéridanh <divintsiela@gmail.com>
2025-08-16 22:25:38 +0200

    add UserResponseDto

---
+++ src/main/java/com/ypareolike/dtos/UserResponseDto.java
@@ -0,0 +1,18 @@
+package com.ypareolike.dtos;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class UserResponseDto {
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private String username;
+    private String email;
+}
