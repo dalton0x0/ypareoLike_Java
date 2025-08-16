aab85ae599fd32e96cfbdbd642dabfc9abf490b3
Chéridanh <divintsiela@gmail.com>
2025-08-16 22:01:00 +0200

    add UserRepository file

---
+++ src/main/java/com/ypareolike/repositories/UserRepository.java
@@ -0,0 +1,7 @@
+package com.ypareolike.repositories;
+
+import com.ypareolike.entities.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+}
